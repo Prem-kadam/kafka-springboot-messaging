e1cfd553a9cc37f31ea74eee2e74e63c1d5ff57e
Prem-kadam <premkadam17015@gmail.com>
2026-06-23 11:28:00 +0530

    Implemented RiderLocation consumer using Spring Cloud Stream

--- consumer/src/main/java/com/example/consumer/Consumer.java
+++ consumer/src/main/java/com/example/consumer/Consumer.java
@@ -1,19 +1,19 @@
 package com.example.consumer;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class Consumer {
-
-    @KafkaListener(topics = "mytopic" ,groupId="mygroup")
-    public void listen(String message){
-        System.out.println("Recieve Message:"+ message);
-    }
-
-    @KafkaListener(topics = "new-topic" ,groupId="new-group")
-    public void listenRider(RiderLocation location){
-        System.out.println("Recieve :"+ location.getRiderId() + "+ " +location.getLatitude() +" +"+location.getLongitude());
-    }
-
-}
+//
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Consumer {
+//
+//    @KafkaListener(topics = "mytopic" ,groupId="mygroup")
+//    public void listen(String message){
+//        System.out.println("Recieve Message:"+ message);
+//    }
+//
+//    @KafkaListener(topics = "new-topic" ,groupId="new-group")
+//    public void listenRider(RiderLocation location){
+//        System.out.println("Recieve :"+ location.getRiderId() + "+ " +location.getLatitude() +" +"+location.getLongitude());
+//    }
+//
+//}
